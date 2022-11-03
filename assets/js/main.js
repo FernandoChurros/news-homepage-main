ccb18385fd902ccff50a4ec3fd9afe206bcc7e54
Fernando Churros <fernando_churros@outlook.com>
2022-11-03 19:41:12 -0300

    refactory logic button using forEach

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,22 @@
-const btn = document.querySelector('#btn-menu');
+// const btn = document.querySelector('#btn-menu');
+// const btnClose = document.querySelector('#btn-menu-close');
 
-btn.addEventListener('click', () => {
+// btn.addEventListener('click', () => {
 
-    btn.classList.toggle('btn__icon-close');
-    document.querySelector('#nav').classList.toggle('hidden');
-    document.querySelector('#opacity').classList.toggle('hidden');
-});
+//     document.querySelector('#nav').classList.remove('hidden');
+//     document.querySelector('#opacity').classList.remove('hidden');
+// });
+
+// btnClose.addEventListener('click', () => {
+
+//     document.querySelector('#nav').classList.add('hidden');
+//     document.querySelector('#opacity').classList.add('hidden');
+// });
+
+document.querySelectorAll('.header__btn').forEach((el) => {
+    el.addEventListener('click', () => {
+
+        document.querySelector('#nav').classList.toggle('hidden');
+        document.querySelector('#opacity').classList.toggle('hidden');
+    })
+})
